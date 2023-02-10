6112e54868d7ab3c6231de9765e34f10808edc00
Your Name <jlmeeks587@gmail.com>
2023-02-09 21:03:29 -0500

    Tour props passed to display image, title and price

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -5,6 +5,12 @@ const Tours = ({tours}) => {
     <section>
       <div className="title">
         <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} />
+        })}
       </div>
     </section>
   );
