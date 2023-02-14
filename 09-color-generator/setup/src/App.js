c7cc3b9306ed4ad8c735f598c70fbc01a995ff85
Your Name <jlmeeks587@gmail.com>
2023-02-13 21:03:30 -0500

    Initial files, state and handleSubmit function

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -4,7 +4,17 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
-}
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
 
+  const handleSubmit =  (e) => {
+    e.preventDefault();
+    console.log('hello');
+  }
+
+  return (
+
+  );
+}
 export default App
