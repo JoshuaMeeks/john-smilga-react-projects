1bbe8e1bea966e4ed5ae91d675069f13c8bad6af
Your Name <jlmeeks587@gmail.com>
2023-02-13 22:02:05 -0500

    conditional added to SingleColor component so that any index greater than 10 get a color-light classname for styling

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -4,12 +4,11 @@ import rgbToHex from './utils'
 const SingleColor = ({rgb, weight, index, hexColor}) => {
   const [alert, setAlert] = useState(false);
   const bcg = rgb.join(',');
-  const hex = rgbToHex(...rgb);
 
   return (
-    <article className={`color`} style={{backgroundColor:`rgb(${bcg})`}}>
+    <article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor:`rgb(${bcg})`}}>
       <p className='percent-value'>{weight}%</p>  
-      <p className="color-value">{hex}</p>
+      <p className="color-value">#{hexColor}</p>
     </article>
   );
 }
