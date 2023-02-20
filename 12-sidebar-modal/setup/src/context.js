302b134ae3498a3b48cf458cce18357864e85215
Your Name <jlmeeks587@gmail.com>
2023-02-19 23:44:10 -0500

    Added state values to context.js

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -4,6 +4,8 @@ const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return <AppContext.Provider value="hello">{children}</AppContext.Provider>
 }
 
