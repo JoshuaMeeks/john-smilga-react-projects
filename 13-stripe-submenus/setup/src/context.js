7a75aa38351d60b41ea5771e572ac2b1c119e5bf
Your Name <jlmeeks587@gmail.com>
2023-02-20 16:39:52 -0500

    set state for sidebar in context.js

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -4,6 +4,8 @@ import sublinks from './data'
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
     <AppContext.Provider>
       {children}
