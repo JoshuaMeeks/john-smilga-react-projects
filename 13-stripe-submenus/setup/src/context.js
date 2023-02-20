053d233e0f5e2e63fd6752ef105a6af8dc922d9d
Your Name <jlmeeks587@gmail.com>
2023-02-20 16:43:56 -0500

    Added functions to context.js

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -5,6 +5,23 @@ const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  }
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  }
+
+   const openModal = () => {
+    setIsModalOpen(true);
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  }
 
   return (
     <AppContext.Provider>
