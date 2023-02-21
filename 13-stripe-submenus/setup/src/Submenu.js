11e0c6a85073e3ac2230bef39581d8b51c53969e
Your Name <jlmeeks587@gmail.com>
2023-02-21 15:45:47 -0500

    Setup ternary operator for aside classname in Submenu

--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -4,7 +4,7 @@ import { useGlobalContext } from './context'
 const Submenu = () => {
   const {isSubmenuOpen} = useGlobalContext();
   return (
-    <aside></aside>
+    <aside className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}></aside>
   );
 }
 
