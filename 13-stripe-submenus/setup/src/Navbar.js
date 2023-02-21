0c9960741a4901b86be9068b4adf44e49bd56365
Your Name <jlmeeks587@gmail.com>
2023-02-21 15:50:43 -0500

    displaySubmenu passed as an event in nav buttons

--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -20,13 +20,13 @@ const Navbar = () => {
         </div>
         <ul className="nav-links">
           <li>
-            <button className="link-btn">products</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>products</button>
           </li>
           <li>
-            <button className="link-btn">developers</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>developers</button>
           </li>
           <li>
-            <button className="link-btn">company</button>
+            <button className="link-btn" onMouseOver={displaySubmenu}>company</button>
           </li>
         </ul>
         <button className="btn signin-btn">sign in</button>
