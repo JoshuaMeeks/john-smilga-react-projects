5425568a8e2660e5d5280a2f71d4ff76cd69eb84
Your Name <jlmeeks587@gmail.com>
2023-02-28 18:34:29 -0500

    throw new Error set up in reducer.js

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -58,7 +58,7 @@ const reducer = (state, action) => {
     }).filter((cartItem) => cartItem.amount !== 0);
     return {...state, cart: tempCart}
   }
-  return state;
+  throw new Error('no matching action type')
 };
 
 export default reducer;
