a1cb9dbac43f1f7d216f2dd106df2bb6f33e1cbc
Your Name <jlmeeks587@gmail.com>
2023-02-28 19:53:44 -0500

    Navbar logo and link set up

--- 15-cocktails/setup/src/components/Navbar.js
+++ 15-cocktails/setup/src/components/Navbar.js
@@ -4,9 +4,13 @@ import logo from '../logo.svg'
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to='/'>
+          <img src={logo} alt="cocktail db logo" className='logo' />
+        </Link>
+      </div>
+    </nav>
   )
 }
 
