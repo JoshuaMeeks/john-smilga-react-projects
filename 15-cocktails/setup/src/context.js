e86601a4d9306cc64efe4386405ee7f54f321056
Your Name <jlmeeks587@gmail.com>
2023-03-01 11:29:10 -0500

    Added to fetchDrink function

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -13,6 +13,8 @@ const AppProvider = ({ children }) => {
     setLoading(true);
     try {
       const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      
     } catch (error) {
       console.log(error);
     }
