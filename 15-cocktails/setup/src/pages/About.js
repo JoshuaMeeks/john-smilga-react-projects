c9232d670d693e3f241b916d7352846713e1b4e2
Your Name <jlmeeks587@gmail.com>
2023-02-28 19:59:11 -0500

    About section created

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Et atque ipsam quia quis at quos impedit excepturi. Repellendus 
+        est error, sequi in magni aliquam culpa voluptates, suscipit 
+        veniam aliquid quo cupiditate? Optio quia porro necessitatibus 
+        sed eveniet illum minus voluptas.
+      </p>
+    </section>
   )
 }
 
