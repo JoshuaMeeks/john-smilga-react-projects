96ccaa780908a820d2c9a787f3af3ab4ce657752
Your Name <jlmeeks587@gmail.com>
2023-03-02 12:40:27 -0500

    input added to SearchForm

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -5,12 +5,21 @@ const SearchForm = () => {
   const {setSearchTerm} = useGlobalContext();
   const searchValue = React.useRef('');
 
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value)
+  }
+
   return (
     <section className="section search">
       <form className="search-form">
         <div className="form-control">
           <label htmlFor="name">search your favorite cocktail</label>
-          <input type="text" id="name" ref={searchValue} />
+          <input 
+            type="text" 
+            id="name" 
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
         </div>
       </form>
     </section>
