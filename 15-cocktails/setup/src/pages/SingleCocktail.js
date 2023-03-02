baaecc51474229c7bacaa1a640d01e495184ee9a
Your Name <jlmeeks587@gmail.com>
2023-03-02 15:06:33 -0500

    SingleCocktail useEffect completed

--- 15-cocktails/setup/src/pages/SingleCocktail.js
+++ 15-cocktails/setup/src/pages/SingleCocktail.js
@@ -15,7 +15,30 @@ const SingleCocktail = () => {
         const response = await fetch(`${url}${id}`)
         const data = await response.json();
         if (data.drinks) {
-
+          const {
+            strDrink:name, 
+            strDrinkThumb:image, 
+            strAlcoholic:info, 
+            strCategory:category, 
+            strGlass:glass,
+            strInstructions:instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = data.drinks[0]
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ]
+          const newCocktail = {
+            name, image, info, category, glass, instructions, ingredients
+          }
+          setCocktail(newCocktail);
         }
         else {
           setCocktail(null);
