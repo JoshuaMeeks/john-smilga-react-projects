a8bef9d5c4def334b552c8036539bebabc943fa5
Your Name <jlmeeks587@gmail.com>
2023-03-02 14:53:18 -0500

    SingleCocktail useEffect

--- 15-cocktails/setup/src/pages/SingleCocktail.js
+++ 15-cocktails/setup/src/pages/SingleCocktail.js
@@ -7,6 +7,17 @@ const SingleCocktail = () => {
   const {id} = useParams();
   const [loading, setLoading] = React.useState(false);
   const [cocktail, setCocktail] = React.useState(null);
+
+  React.useEffect(() => {
+    setLoading(true);
+    async function getCocktail() {
+      try {
+        const response = await fetch(`${url}${id}`)
+      } catch (error) {
+        
+      }
+    }
+  }, [id])
   return (
     <div>
       <h2>{id}</h2>
